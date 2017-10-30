a7b3e3d09de66c1f655d6976ad83530f3a869275
Sergey Zhuravlev <sergey4zhuravlev@gmail.com>
2017-10-31 00:33:20 +0300

    Добавьте файлы проекта.

---
+++ AssistantBot/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssistantBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
